fe0e6b1b4bfb9921feb286031d33d9e6c4ea3819
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-04-03 11:11:22 +0200

    initial

---
+++ src/main/java/org/workshop/meetingcalendarapi/MeetingCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package org.workshop.meetingcalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalendarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingCalendarApiApplication.class, args);
+    }
+
+}
