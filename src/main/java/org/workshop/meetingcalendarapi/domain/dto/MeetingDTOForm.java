b72e2e8d46b7d2b0128204336828c3efaf0caf8c
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-04-04 13:42:10 +0200

    Can add meetings from postman, service layer added, dependency for validation updated

---
+++ src/main/java/org/workshop/meetingcalendarapi/domain/dto/MeetingDTOForm.java
@@ -0,0 +1,20 @@
+package org.workshop.meetingcalendarapi.domain.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import org.workshop.meetingcalendarapi.domain.model.MeetingLevel;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record MeetingDTOForm(
+        @NotBlank(message = "Title is required")
+        String title,
+        @NotBlank(message = "Date is required")
+        LocalDate date,
+        @NotBlank(message = "Time is required")
+        LocalTime time,
+        @NotBlank(message = "Meeting level is required")
+        MeetingLevel level,
+        String participants,
+        String description) {}
---
+++ src/main/java/org/workshop/meetingcalendarapi/service/MeetingService.java
@@ -0,0 +1,33 @@
+package org.workshop.meetingcalendarapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.workshop.meetingcalendarapi.domain.dto.MeetingDTOForm;
+import org.workshop.meetingcalendarapi.domain.entity.Meeting;
+import org.workshop.meetingcalendarapi.repository.MeetingsRepository;
+
+@Service
+public class MeetingService {
+
+    private MeetingsRepository meetingsRepository;
+
+    @Autowired
+    public MeetingService(MeetingsRepository meetingsRepository) {
+        this.meetingsRepository = meetingsRepository;
+    }
+
+
+    public void createMeeting(MeetingDTOForm form) {
+        Meeting meeting = Meeting.builder()
+                .title(form.title())
+                .description(form.description())
+                .level(form.level())
+                .date(form.date())
+                .time(form.time())
+                .participants(form.participants())
+                .description(form.description())
+                .build();
+        meetingsRepository.save(meeting);
+    }
+
+}
